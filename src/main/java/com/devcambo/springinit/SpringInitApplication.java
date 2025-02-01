aa6fcb9f7cd6b8310d244256b1422414c8fe6760
devcambo <devcambo001@gmail.com>
2025-02-01 09:36:24 +0700

    Fixed pagination warning.

--- src/main/java/com/devcambo/springinit/SpringInitApplication.java
+++ src/main/java/com/devcambo/springinit/SpringInitApplication.java
@@ -3,9 +3,13 @@ package com.devcambo.springinit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class SpringInitApplication {
 
   public static void main(String[] args) {
